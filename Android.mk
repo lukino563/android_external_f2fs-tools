9128730e891d5d1982d8dd4220f0edc5918dd35e
lukino563 <lukino563@gmail.com>
2016-11-07 23:08:17 +0100

    F2FS: Forced static executables

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ ifneq (,$(filter linux darwin,$(HOST_OS)))
 global_CFLAGS := -DWITH_ANDROID -DWITH_BLKDISCARD -DF2FS_MAJOR_VERSION=1 -DF2FS_MINOR_VERSION=7 -DF2FS_TOOLS_VERSION=\"1.7.0\" -DF2FS_TOOLS_DATE=\"2016-11-05\"
 
 # external/e2fsprogs/lib is needed for uuid/uuid.h
-common_C_INCLUDES := $(LOCAL_PATH)/include external/e2fsprogs/lib/ $(LOCAL_PATH)/mkfs
+common_C_INCLUDES := $(LOCAL_PATH)/include external/e2fsprogs/lib/
 
 #----------------------------------------------------------
 libf2fs_src_files := lib/libf2fs.c lib/libf2fs_io.c lib/libf2fs_zoned.c
@@ -39,8 +39,9 @@ LOCAL_SRC_FILES := $(mkfs_f2fs_src_files)
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
 LOCAL_CFLAGS := $(global_CFLAGS)
 LOCAL_CLANG := false
-LOCAL_SHARED_LIBRARIES := libf2fs libext2_uuid
+LOCAL_STATIC_LIBRARIES := libc libf2fs_static libext2_uuid_static
 LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -70,8 +71,9 @@ LOCAL_MODULE := fsck.f2fs
 LOCAL_SRC_FILES := $(fsck_f2fs_src_files)
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
 LOCAL_CFLAGS := $(global_CFLAGS)
-LOCAL_SHARED_LIBRARIES := libf2fs
+LOCAL_STATIC_LIBRARIES := libc libf2fs_static libext2_uuid_static
 LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -83,12 +85,15 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_LIBRARY)
 
 #----------------------------------------------------------
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := f2fstat
 LOCAL_SRC_FILES := tools/f2fstat.c
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
 LOCAL_CFLAGS := $(global_CFLAGS)
+LOCAL_STATIC_LIBRARIES := libc
 LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -96,7 +101,9 @@ LOCAL_MODULE := fibmap.f2fs
 LOCAL_SRC_FILES := tools/fibmap.c
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
 LOCAL_CFLAGS := $(global_CFLAGS)
+LOCAL_STATIC_LIBRARIES := libc
 LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -104,10 +111,13 @@ LOCAL_MODULE := parse.f2fs
 LOCAL_SRC_FILES := tools/f2fs_io_parse.c
 LOCAL_C_INCLUDES := $(common_C_INCLUDES)
 LOCAL_CFLAGS := $(global_CFLAGS)
+LOCAL_STATIC_LIBRARIES := libc
 LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 #----------------------------------------------------------
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libf2fs_fmt_host
 LOCAL_SRC_FILES := \
